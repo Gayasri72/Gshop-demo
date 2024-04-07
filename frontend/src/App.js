784e2838d0d9984bfd757e0ca74e27e9eee1bf33
Gayasri72 <gayasripethum1@gmail.com>
2024-04-07 19:11:29 +0530

    update routes

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,20 +1,21 @@
-import React from 'react'
-import Header from './components/Header'
-import {Container} from "react-bootstrap"
-import Footer from './components/Footer'
+import React from "react";
+import Header from "./components/Header";
+import { Container } from "react-bootstrap";
+import Footer from "./components/Footer";
+import { Outlet } from "react-router-dom";
 
 const App = () => {
   return (
-   <>
-   <Header/>
-   <main className='py-3'>
-    <Container>
-      <h1>Welcome to Gshop</h1>
-    </Container>
-   </main>
-   <Footer/>
-   </>
-  )
-}
+    <>
+      <Header />
+      <main className="py-3">
+        <Container>
+          <Outlet/>
+        </Container>
+      </main>
+      <Footer />
+    </>
+  );
+};
 
-export default App
+export default App;
